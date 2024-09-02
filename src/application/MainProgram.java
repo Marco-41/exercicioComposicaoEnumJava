6f2bd9f254f7d497149a6f4dde14833a4deadd9d
Marco Oliveira <marco_oliveira98@outlook.com>
2024-09-02 11:11:12 -0300

    Update na classe Client: Atributos, Construtores, Getters e Stters, e criação do método toString para impressão dos dados do cliente.

--- src/application/MainProgram.java
+++ src/application/MainProgram.java
@@ -1,17 +1,23 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Client;
 import entities.OrderItem;
 import entities.Product;
 
 public class MainProgram {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		//INSTANCIANDO UM SimpleDateFormat PARA FORMATAÇÃO DE DATA.
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		//INSTANCIANDO UM Product PARA TESTAR.
 		Product p = new Product("Smart TV", 1000.00);
@@ -19,8 +25,14 @@ public class MainProgram {
 		//INSTANCIANDO UM OrderItem PARA TESTAR.
 		OrderItem oi1 = new OrderItem(1, 1000.00, p);
 		
+		//INSTANCIANDO UM Client PARA TESTAR.
+		Client c = new Client("Marco Oliveira", "marcooliveira@gmail.com", sdf.parse("19/06/1998"));
+		
 		//IMPRIMINDO O PRODUTO PARA TESTAR.
 		System.out.println(oi1);
+		
+		//IMPRIMINDO O Client PARA TESTAR.
+		System.out.println(c);
 		
 		sc.close();
 	}
--- src/entities/Client.java
+++ src/entities/Client.java
@@ -1,5 +1,60 @@
 package entities;
 
-public class Client {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class Client {
+	
+	//INSTANCIANDO UM SimpleDateFormat PARA FORMATAÇÃO DE DATA.
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+	//DECLARAÇÃO DOS ATRIBUTOS.
+	private String name;
+	private String email;
+	private Date birthDate;
+	
+	//CRIAÇÃO DO CONSTRUTOR PADRÃO VAZIO.
+	public Client() {
+		
+	}
+
+	//CRIAÇÃO DO CONSTRUTOR COM ARGUMENTOS.
+	public Client(String name, String email, Date birthDate) {
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+	}
+
+	//CRIAÇÃO DOS GETTERS E SETTERS.
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	//CRIAÇÃO DO MÉTODO toString PARA IMPRESSÃO DOS DADOS.
+	@Override
+	public String toString() {
+		return "Nome: " + name + "\n" +
+				"Data de nascimento: " + "(" + sdf.format(birthDate) + ")" + "\n" +
+				"Email: " + email;
+	}
 }
