2f1ee6bf1e6f8281ead336e3b5ac137857be4b9a
Marco Oliveira <marco_oliveira98@outlook.com>
2024-09-02 09:42:47 -0300

    Update na classe OrderItem.

--- src/entities/OrderItem.java
+++ src/entities/OrderItem.java
@@ -2,4 +2,48 @@ package entities;
 
 public class OrderItem {
 
+	//DECLARAÇÃO DE ATRIBUTOS.
+	private Integer quantity;
+	private Double price;
+	
+	//CRIAÇÃO DA COMPOSIÇÃO ENTRE UM OBJETO E OUTRO (OrderItem CONTÉM UMA REFERÊNCIA A UM PRODUTO).
+	private Product product; 
+	
+	//CRIAÇÃO DO CONSTRUTOR PADRÃO VAZIO.
+	public OrderItem() {
+		
+	}
+
+	//CRIAÇÃO DO CONSTRUTOR COM ARGUMENTOS.
+	public OrderItem(Integer quantity, Double price, Product product) {
+		this.quantity = quantity;
+		this.price = price;
+		this.product = product;
+	}
+	
+	//CRIAÇÃO DOS GETTERS E SETTERS,
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
 }
